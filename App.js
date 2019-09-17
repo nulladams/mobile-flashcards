7538946e057aba97d34b26900b9182596f276b14
Leonardo Adams <leonardoadams@gmail.com>
2019-09-17 15:12:54 -0300

    Create Redux store and provite it to the app

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, StatusBar } from 'react-native'
+import { createStore } from 'redux'
+import reducer from './reducers'
+import { Provider } from 'react-redux'
+import AddDeckView from './components/AddDeckView'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={createStore(reducer)}>
+        <View style={styles.container}>
+          <AddDeckView />
+        </View>
+      </Provider>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+})
