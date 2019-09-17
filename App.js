6d5d13cdebb6c70b9f86176b084bc774d50a996e
Leonardo Adams <leonardoadams@gmail.com>
2019-09-17 18:18:30 -0300

    Tabs navigation

--- App.js
+++ App.js
@@ -1,11 +1,57 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, View, StatusBar } from 'react-native'
+import { createAppContainer } from 'react-navigation'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { createStackNavigator }from 'react-navigation-stack'
 import { createStore } from 'redux'
 import reducer from './reducers'
 import { Provider } from 'react-redux'
 import Constants from 'expo-constants'
+import { FontAwesome } from '@expo/vector-icons'
 import AddDeckView from './components/AddDeckView'
-import { purple } from './utils/colors'
+import DecksListView from './components/DecksListView'
+import { purple, white } from './utils/colors'
+
+const Tabs = createBottomTabNavigator({
+  DecksListView: {
+    screen: DecksListView,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='th-list' size={30} color={tintColor}  />
+    }
+  },
+  AddDeckView: {
+    screen: AddDeckView,
+    navigationOptions: {
+      tabBarLabel: 'Add Deck',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    }
+  }
+}, {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: white,
+    style: {
+      height: 56,
+      backgroundColor: purple,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
+
+const MainNavigator = createAppContainer(createStackNavigator({
+  Home: {
+    screen: Tabs
+  }
+}))
 
 function AppStatusBar ({ backgroundColor, ...props }) {
   return (
@@ -15,14 +61,13 @@ function AppStatusBar ({ backgroundColor, ...props }) {
   )
 }
 
-
 export default class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={styles.container}>
           <AppStatusBar backgroundColor={purple} barStyle='light-content' />
-          <AddDeckView />
+          <MainNavigator />
         </View>
       </Provider>
     )
