dcee00d5dce577e9270fb28061d526188f23ae46
Leonardo Adams <leonardoadams@gmail.com>
2019-09-23 10:16:04 -0300

    Game logic

--- components/QuizView.js
+++ components/QuizView.js
@@ -3,36 +3,111 @@ import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import { connect } from 'react-redux'
 import { green, red } from '../utils/colors'
 
+const CorrectBtn = (props) => {
+    return (
+        <TouchableOpacity
+            style={styles.androidCorrectBtn}
+            onPress={() => props.onPress(true)}
+        >
+            <Text style={{ textAlign: 'center' }}>Correct</Text>
+        </TouchableOpacity>
+    )
+}
+
+const IncorrectBtn = (props) => {
+    return (
+        <TouchableOpacity
+            style={styles.androidIncorrectBtn}
+            onPress={() => props.onPress(false)}
+        >
+            <Text style={{ textAlign: 'center' }}>Incorrect</Text>
+        </TouchableOpacity>
+    )
+}
+
 class QuizView extends Component {
+    static navigationOptions = ({ navigation }) => {
+        return {
+            title: 'Quiz'
+        }
+    }
+    state = {
+        count: 0,
+        toggle: false,
+        correctAnswers: 0
+    }
+    toggleCard = (answerGiven) => {
+        this.setState((currState) => ({
+            toggle: !currState.toggle    
+        }))
+    }
+    checkAnswer = (answerGiven) => {
+        const { count } = this.state
+        const { isAnswerCorrect } = this.props.deck.questions[count]
+
+        this.setState((currState) => ({
+            count: currState.count + 1,
+            toggle: false,
+            correctAnswers: (answerGiven === isAnswerCorrect) 
+                ? currState.correctAnswers + 1
+                : currState.correctAnswers
+        }))
+    }
     render() {
+        const { count, toggle, correctAnswers } = this.state
         const { deck } = this.props
-        return (
-            <View style={{ flex: 1 }}>
-                <View style={{ flex: 1, padding: 10, fontSize: 20 }}>
-                    <Text>Quiz View</Text>
+        if (deck.questions.length === 0) {
+            return (
+                <View style={styles.container}>
+                    <Text style={{ fontSize: 20 }}>No questions in this deck</Text>
                 </View>
-                <View style={styles.questionsContainer}>
-                    <Text style={{ fontSize: 30 }}>Question</Text>
-                    <TouchableOpacity
-                        style={{ height: 45 }}
-                    >
-                        <Text style={{ fontSize: 20, color: red }}>Answer</Text>
-                    </TouchableOpacity>
+            )
+        }
+        if (deck.questions.length > 0 && count < deck.questions.length) {
+            return (
+                <View style={{ flex: 1 }}>
+                    <View style={{ flex: 1, padding: 10, fontSize: 20 }}>
+                        <Text style={{ padding: 10, fontSize: 15 }}>{count + 1} of {deck.questions.length}</Text>
+                    </View>
+                    {toggle
+                        ? (
+                            <View style={styles.questionsContainer}>
+                                <Text style={{ fontSize: 30 }}>{deck.questions[count].answer}</Text>
+                                <TouchableOpacity
+                                    style={{ height: 45 }}
+                                    onPress={this.toggleCard}
+                                >
+                                    <Text style={{ fontSize: 20, color: red }}>Question</Text>
+                                </TouchableOpacity>
+                            </View>
+                        )
+                        : (
+                            <View style={styles.questionsContainer}>
+                                <Text style={{ fontSize: 30 }}>{deck.questions[count].question}</Text>
+                                <TouchableOpacity
+                                    style={{ height: 45 }}
+                                    onPress={this.toggleCard}
+                                >
+                                    <Text style={{ fontSize: 20, color: red }}>Answer</Text>
+                                </TouchableOpacity>
+                            </View>
+                        )
+                    }
+                    
+                    <View style={styles.btnsContainer}>
+                        <CorrectBtn onPress={this.checkAnswer} />
+                        <IncorrectBtn onPress={this.checkAnswer} />
+                    </View>
                 </View>
-                <View style={styles.btnsContainer}>
-                    <TouchableOpacity
-                        style={styles.androidCorrectBtn}
-                    >
-                        <Text style={{ textAlign: 'center' }}>Correct</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={styles.androidIncorrectBtn}
-                    >
-                        <Text style={{ textAlign: 'center' }}>Incorrect</Text>
-                    </TouchableOpacity>
+            )
+        } else {
+            return (
+                <View style={styles.container}>
+                    <Text style={{ fontSize: 25, textAlign: 'center' }}>Number of correct answers: {correctAnswers} of {deck.questions.length}</Text>
                 </View>
-            </View>
-        )
+            )
+        }
+
     }
 }
 
